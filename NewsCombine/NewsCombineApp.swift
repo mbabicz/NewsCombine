b9ddb36f0ef11f2c31aac1a9041a73c1fe67571f
Michał Babicz <kiwuzx@gmail.com>
2023-03-16 13:51:42 +0100

    Initial Commit

---
+++ NewsCombine/NewsCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsCombineApp.swift
+//  NewsCombine
+//
+//  Created by kz on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
